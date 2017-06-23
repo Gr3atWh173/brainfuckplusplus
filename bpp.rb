e99f7b6c63fcfe8210749f8fd4c0f0042fe0d456
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2017-06-23 18:19:38 +0530

    Implemented standard brainfuck

---
+++ bpp.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+#require 'benchmark'
+
+# A list of allowed charachters.
+ALLOWED = ['.', ',', '[', ']', '<', '>', '+', '-']
+
+def execute(filename)
+	begin    
+		f = File.read(filename)
+	rescue
+		puts "No such file."
+		exit
+	end
+	
+    evaluate(f)
+
+end
+
+def evaluate(code)
+    # cleanup the code (Remove anything not in ALLOWED) and then build a bracemap.
+    code = cleanup(code.chars)
+    bracemap = buildbracemap(code)
+
+    cells, codeptr, cellptr = [0], 0, 0
+
+    while codeptr < code.length
+        command = code[codeptr]
+
+        # Iterate through all the commands in the code and evaluate
+        case command
+        when '>' then cellptr += 1
+                      cells.push(0) if cellptr == cells.length
+        when '<' then cellptr = cellptr <= 0 ? 0 : cellptr - 1
+        when '+' then cells[cellptr] = cells[cellptr] < 255 ? cells[cellptr] + 1 : 0
+        when '-' then cells[cellptr] = cells[cellptr] > 0 ? cells[cellptr] - 1 : 255
+        when "[" then if cells[cellptr] == 0 then codeptr = bracemap[codeptr] end
+        when "]" then if cells[cellptr] != 0 then codeptr = bracemap[codeptr] end
+        when "." then $stdout.write cells[cellptr].chr
+        when ',' then cells[cellptr] = gets.chomp.chr.ord
+        end
+
+        codeptr += 1
+    end
+end
+
+def cleanup(code)
+    cleaned = []
+    code.each do |c|
+        if ALLOWED.include? c then cleaned.push(c) end
+    end
+    return cleaned
+end
+    
+def buildbracemap(code)
+    temp_bracestack, bracemap = [], {}
+
+    code.each_with_index do |command, position|
+        if command == "[" then temp_bracestack.push(position) end
+        if command == "]"
+            start = temp_bracestack.pop
+            bracemap[start] = position
+            bracemap[position] = start
+        end
+
+    end
+    return bracemap
+end
+
+def main()
+    if ARGV.length == 1 then execute(ARGV[0])
+    else print "Usage: #{File.basename($0)} filename" end
+end
+
+#puts Benchmark.measure{main}
+main
+exit
